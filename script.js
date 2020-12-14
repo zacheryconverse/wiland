5d0cde3c0eff00614ef2fcb163a095307047cffd
Zachery Converse <zacheryconverse@gmail.com>
2020-12-14 11:17:09 -0700

    Progress so far

---
+++ script.js
@@ -0,0 +1,13 @@
+const symbols = document.getElementsByClassName('symbol');
+const numbers = document.getElementsByClassName('number');
+const equals = document.getElementsByClassName('equals');
+
+// set initial 'to be calculated' state to empty string
+// set calculated state to empty string
+// update state onChange
+
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+    // set button.innerText
+  });
+});
